138a6b488214f662b0d79011fe26388d5133ab37
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-03-15 21:46:05 -0700

    adds javascript to dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,31 @@
+console.log("it works");
+
+var addPatientForm = document.forms.addPatientForm;
+
+addPatientForm.addEventListener("submit", function(e) {
+    addPatient(e);
+});
+
+var patientArrays = [];
+
+function addPatient(e) {
+    e.preventDefault();
+    var patientUl = document.querySelector("#patient-list");
+    var addInput = document.querySelector("#patientName").value;
+
+    var patientLi = document.createElement("li");
+    var patientBtn = document.createElement("button");
+    patientBtn.classList.add("btn", "btn-block", "made-patient-btn");
+    patientBtn.setAttribute("type", "submit");
+    patientBtn.setAttribute("value", addInput);
+
+    console.log(addInput);
+
+    patientUl.appendChild(patientLi);
+
+    patientArrays.push(addInput);
+    console.log(patientArrays);
+
+    patientLi.appendChild(patientBtn);
+    patientBtn.innerHTML = addInput;
+}
